9b1d8333d18e17d59b68d3d33392f61021a11053
Antoine Paix <antoine.paix@gmail.com>
2019-04-11 16:24:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# downloader4chan
+
+This simple bash script downloads the entire content of a 4chan's thread
+
+### Prerequisites
+
+To run this script you need to install curl and wget commands
+On Ubuntu/Debian distro type in terminal :
+```
+sudo apt install curl wget
+```
+### Installing
+
+```
+git clone https://github.com/AntoinePaix/downloader4chan
+cd downloader4chan
+chmod +x downloader_4chan.sh
+```
+
+### How to use
+
+```
+./downloader_4chan.sh <url_4chan_thread>
+```
+
+Files are saved in the 4chan folder in your home directory.
+
+Enjoy !
---
+++ downloader_4chan.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# DESCRIPTION: This simple bash script downloads the entire content of a 4chan's thread
+# @author: Antoine Paix
+# @date: 2019/04/10
+
+# To run this script you need to install curl and wget commands
+# On Ubuntu/Debian distro type in terminal: sudo apt install curl wget
+# Run chmod +x on this script to make it executable
+# Enjoy !
+
+shopt -s extglob
+
+# URL 4chan thread
+THREAD_URL="${1}"
+
+# Folder where the content are saved (you can modified the path if you want)
+PATH_TO_FOLDER="/home/$USER/4chan"
+
+FOLDER_NAME=$(basename "$1")
+CONTENT_FOLDER="${PATH_TO_FOLDER}/${FOLDER_NAME}"
+
+usage() {
+    echo "DESCRIPTION: this script downloads the entire content of a thread on 4chan.org website"
+    echo "USAGE: bash $(basename ${0}) <url_4chan_thread>"
+}
+
+if [ $# -ne 1 ]
+then
+    usage
+    exit 1
+fi
+
+# Url control
+invalid_url() {
+    echo "INVALID URL: URL must have this format (example): https://boards.4channel.org/g/thread/20558626"
+}
+
+regex='https://boards.4channel.org/[[:alnum:]]*/thread/[[:digit:]]*'
+if [[ $THREAD_URL =~ $regex ]]
+then
+    echo "[+] Good URL: ${THREAD_URL}"
+else
+    invalid_url && exit 2
+fi
+
+# Creating folder
+if [ ! -d "${CONTENT_FOLDER}" ]
+then
+    echo "[+] Creating the folder "${CONTENT_FOLDER}""
+    mkdir -p "${CONTENT_FOLDER}"
+else
+    echo "[+] Downloading content to the folder ${CONTENT_FOLDER}"
+fi
+
+cd $CONTENT_FOLDER
+
+
+for file in $(curl -s "${THREAD_URL}" | tr " " "\n" | grep "href=\"//i.4cdn.org/*/[[:digit:]]*.[[:lower:]]*" | \
+                cut -d'=' -f2 | sed -e 's/"//g ; s/\/\///g' | uniq)
+do
+    FILE="https://"${file}""
+
+    # echo "${FILE}" # use for debugging only
+    
+    wget -nc "${FILE}"
+
+done
+
+if [ "$?" -eq 0 ]
+then
+    echo "[+] Files are saved in "${CONTENT_FOLDER}""
+fi
